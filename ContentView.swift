ad989fe8cdc60b92c83e90a4e68391ec9941e486
StarryZhang-whu <93083801+StarryZhang-whu@users.noreply.github.com>
2023-03-16 17:08:05 +0800

    Build framework,Add TodoView
    some related work.
    build a static page.

--- ContentView.swift
+++ ContentView.swift
@@ -1,21 +1,37 @@
 //
-//  ContentView.swift
+//  TabBar.swift
 //  Domain_iOS
 //
-//  Created by 张凌浩 on 2023/3/15.
+//  Created by 张凌浩 on 2023/3/16.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView{
+            TodoView()
+                .tabItem(){
+                    Image(systemName: "list.bullet.below.rectangle")
+                    Text("事项")
+                }
+            TodoView()
+                .tabItem(){
+                    Image(systemName: "newspaper")
+                    Text("新闻")
+                }
+            TodoView()
+                .badge(5)
+                .tabItem(){
+                    Image(systemName: "circle.circle.fill")
+                    Text("圈子")
+                }
+            TodoView()
+                .tabItem(){
+                    Image(systemName: "person.circle")
+                    Text("我的")
+                }
         }
-        .padding()
     }
 }
 
