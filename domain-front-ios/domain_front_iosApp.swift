e7adbb819e44ac45b6dd734e1f7af564ac953692
SunnyVulpes <1791774501@qq.com>
2023-03-27 16:30:00 +0800

    Initial Commit

---
+++ domain-front-ios/domain_front_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  domain_front_iosApp.swift
+//  domain-front-ios
+//
+//  Created by 韩旭 on 2023/3/27.
+//
+
+import SwiftUI
+
+@main
+struct domain_front_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
