c52e762ef18eb84378a6b23a9e8041b05759e605
SunnyVulpes <1791774501@qq.com>
2023-03-29 10:35:25 +0800

    test

--- Views/Main/TodoView.swift
+++ Views/Main/TodoView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct TodoView: View {
-<<<<<<< Updated upstream
 =======
     @StateObject var todoshop:TodoShop = TodoShop()
 >>>>>>> Stashed changes
@@ -21,33 +20,27 @@ struct TodoView: View {
                     .padding(.top,20)
                 VStack(){
                     HStack {
-<<<<<<< Updated upstream
                         Image(systemName: "envelope.fill").foregroundColor(.accentColor)
                             .frame(height: 30)
                         .background(Rectangle().fill(.background))
-=======
                         Image(systemName: "envelope.fill")
                             .foregroundColor(.accentColor)
                             .frame(height: 30)
                             .background(Rectangle()
                                 .fill(.background)
                             )
->>>>>>> Stashed changes
                         Text("**3**").foregroundColor(.accentColor)
                     }.frame(width: 100)
                 }.frame(maxWidth: .infinity,alignment: .trailing)
                 
-<<<<<<< Updated upstream
                 DaysView(todoList: todos[0], title: "今天")
                 DaysView(todoList: todos[1], title: "明天")
                 DaysView(todoList: todos[2], title: "3月14日")
-=======
                 DaysView(todoshop: todoshop, title: "今天")
                     .padding(.bottom, 20)
                     .sheet(isPresented: $todoshop.isEditing){
                         ItemDetail(todoshop: todoshop, locate: todoshop.editingAt)
                     }
->>>>>>> Stashed changes
             }
             addButton(size: 60).offset(x: UIScreen.main.bounds.width/2 - 60, y: UIScreen.main.bounds.height/2 - 180)
         }
