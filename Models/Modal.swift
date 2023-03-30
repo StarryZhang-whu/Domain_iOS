8d84902b0b827a71886b0ff06f6cbcbf0b4f8075
StarryZhang-whu <93083801+StarryZhang-whu@users.noreply.github.com>
2023-03-31 02:15:57 +0800

    Update Account

--- Models/Modal.swift
+++ Models/Modal.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 class Modal: ObservableObject {
-    @Published var isLogin = false
+    @Published var isLogin: Bool = false{
+        didSet{
+            UserDefaults.standard.set(isLogin, forKey: "isLogin")
+        }
+    }
     @Published var showModal = false
     @Published var selectedModal:ModalEnum = .login
     @Published var user = User(name: "傻逼", bio: "我是一个大傻逼", avator: "https://c-ssl.duitang.com/uploads/item/202005/24/20200524124313_mNiVw.thumb.400_0.jpeg")
+    init(){
+        isLogin = UserDefaults.standard.bool(forKey: "isLogin")
+    }
 }
 
 enum ModalEnum: String {
---
+++ Views/Components/AvatorView.swift
@@ -0,0 +1,36 @@
+//
+//  AvatorView.swift
+//  Domain_iOS
+//
+//  Created by 张凌浩 on 2023/3/31.
+//
+
+import SwiftUI
+
+struct AvatorView: View {
+    @EnvironmentObject var modal:Modal
+    var body: some View {
+        AsyncImage(url: URL(string: modal.user.avator)) {
+            phase in
+            switch phase {
+            case .success(let image):
+                image.resizable()
+            case .empty:
+                ProgressView()
+            case .failure(_):
+                Color.gray
+            @unknown default:
+                EmptyView()
+            }
+        }
+        .frame(width: 50,height: 50)
+        .cornerRadius(20)
+    }
+}
+
+struct AvatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        AvatorView()
+            .environmentObject(Modal())
+    }
+}
